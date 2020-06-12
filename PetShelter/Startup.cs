65930c796aef49498ecd19973c3356efe2082b5a
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-06-12 10:57:22 -0700

    Add Swagger for API documentation/functionality

--- PetShelter/Startup.cs
+++ PetShelter/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace PetShelter
 {
@@ -23,6 +24,10 @@ namespace PetShelter
       services.AddDbContext<PetShelterContext>(opt =>
           opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+      services.AddSwaggerGen(c =>
+        {
+          c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pet Shelter API", Version = "v1", Description = "A simple API created to test backend API understanding for Epicodus' C#/.Net course." });
+        });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,7 +42,12 @@ namespace PetShelter
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
       }
-
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+        {
+          c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pet Shelter API V1");
+          c.RoutePrefix = string.Empty;
+        });
       // app.UseHttpsRedirection();
       app.UseMvc();
     }
